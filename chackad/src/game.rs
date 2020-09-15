65a7f6a0b5cc626fbc8731db2dc02b4122f8a30e
Björn Thorén <bthoren@kth.se>
2020-09-15 09:54:41 +0200

    wip

--- chackad/src/game.rs
+++ chackad/src/game.rs
@@ -5,17 +5,21 @@ use std::collections::HashMap;
 use std::collections::HashSet;
 
 pub struct Game {
-    all_pieces: HashSet<Piece>,
+    white_pieces: HashSet<Piece>,
+    black_pieces: HashSet<Piece>,
     board: Board,
     all_moves: HashMap<(usize, usize), (usize, usize)>,
+    whites_turn: bool,
 }
 
 impl Game {
     pub fn new() -> Game {
         Game {
-            all_pieces: HashSet::new(),
+            white_pieces: HashSet::new(),
+            black_pieces: HashSet::new(),
             board: Board::new(8, 8),
             all_moves: HashMap::new(),
+            whites_turn: true,
         }
     }
 
@@ -24,52 +28,59 @@ impl Game {
 
         // White pieces
         for i in 0..8 {
-            self.all_pieces
+            self.white_pieces
                 .insert(Piece::new(&mut self.board, PieceType::Pawn, i, 1, true));
         }
-        self.all_pieces
+        self.white_pieces
             .insert(Piece::new(&mut self.board, PieceType::Rook, 0, 0, true));
-        self.all_pieces
+        self.white_pieces
             .insert(Piece::new(&mut self.board, PieceType::Knight, 1, 0, true));
-        self.all_pieces
+        self.white_pieces
             .insert(Piece::new(&mut self.board, PieceType::Bishop, 2, 0, true));
-        self.all_pieces
+        self.white_pieces
             .insert(Piece::new(&mut self.board, PieceType::Queen, 3, 0, true));
-        self.all_pieces
+        self.white_pieces
             .insert(Piece::new(&mut self.board, PieceType::King, 4, 0, true));
-        self.all_pieces
+        self.white_pieces
             .insert(Piece::new(&mut self.board, PieceType::Bishop, 5, 0, true));
-        self.all_pieces
+        self.white_pieces
             .insert(Piece::new(&mut self.board, PieceType::Knight, 6, 0, true));
-        self.all_pieces
+        self.white_pieces
             .insert(Piece::new(&mut self.board, PieceType::Rook, 7, 0, true));
 
         // Black pieces
         for i in 0..8 {
-            self.all_pieces
+            self.black_pieces
                 .insert(Piece::new(&mut self.board, PieceType::Pawn, i, 6, false));
         }
-        self.all_pieces
+        self.black_pieces
             .insert(Piece::new(&mut self.board, PieceType::Rook, 0, 7, false));
-        self.all_pieces
+        self.black_pieces
             .insert(Piece::new(&mut self.board, PieceType::Knight, 1, 7, false));
-        self.all_pieces
+        self.black_pieces
             .insert(Piece::new(&mut self.board, PieceType::Bishop, 2, 7, false));
-        self.all_pieces
+        self.black_pieces
             .insert(Piece::new(&mut self.board, PieceType::Queen, 3, 7, false));
-        self.all_pieces
+        self.black_pieces
             .insert(Piece::new(&mut self.board, PieceType::King, 4, 7, false));
-        self.all_pieces
+        self.black_pieces
             .insert(Piece::new(&mut self.board, PieceType::Bishop, 5, 7, false));
-        self.all_pieces
+        self.black_pieces
             .insert(Piece::new(&mut self.board, PieceType::Knight, 6, 7, false));
-        self.all_pieces
+        self.black_pieces
             .insert(Piece::new(&mut self.board, PieceType::Rook, 7, 7, false));
     }
 
     fn calc_all_moves(&mut self) {
         self.all_moves = HashMap::new();
-        for a in self.all_pieces.iter() {
+        let mut turn = HashSet::new();
+
+        if self.whites_turn {
+            turn = self.white_pieces;
+        } else {
+            turn = self.black_pieces;
+        }
+        for a in turn.iter() {
             for i in 0..self.board.size_x {
                 for j in 0..self.board.size_y {
                     if a.check_to(i, j, &self.board, PieceType::Pawn) {
