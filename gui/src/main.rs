f2ce69df7b195309fc440892ba799efe5b6f9589
antlilja <liljaanton2001@gmail.com>
2020-09-23 10:53:00 +0200

    Piston minimal chess Pieces can be selected and moves can be displayed

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -1,3 +1,20 @@
+extern crate piston_window;
+
+use piston_window::*;
+
+mod application;
+
+use application::Application;
+
 fn main() {
-    println!("Hello gui!");
+    let mut window: PistonWindow = WindowSettings::new("Chess", [1400, 1000])
+        .exit_on_esc(true)
+        .resizable(false)
+        .vsync(true)
+        .build()
+        .unwrap();
+
+    let mut app = Application::new(&mut window.create_texture_context());
+
+    app.run(&mut window);
 }
