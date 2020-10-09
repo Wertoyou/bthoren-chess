5cf5e75d1bde417d72aeb86e4448006e1ec30b09
antlilja <liljaanton2001@gmail.com>
2020-10-09 13:29:45 +0200

    Added basic multiplayer functionality

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -12,6 +12,10 @@ use ggez::{Context, GameResult};
 use chackad::game::Game;
 use chackad::piece::{Piece, PieceType};
 
+use std::env;
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
+
 struct Assets {
     chessboard: graphics::Image,
     pieces: Vec<graphics::Image>,
@@ -70,6 +74,13 @@ impl Assets {
     }
 }
 
+#[derive(PartialEq)]
+enum State {
+    Playing,
+    WaitingForOpponent,
+    GameOver,
+}
+
 struct GameState {
     game: Game,
     selected_tile: Option<(usize, usize)>,
@@ -80,16 +91,32 @@ struct GameState {
     board_size: f32,
     tile_size: f32,
     scale_factor: f32,
+
+    state: State,
+
+    // Networking
+    next_move: Option<((usize, usize), (usize, usize, PieceType))>,
+    stream: Option<TcpStream>,
 }
 
 impl GameState {
-    fn new(ctx: &mut Context) -> GameResult<GameState> {
+    fn new(ctx: &mut Context, stream: Option<TcpStream>, client: bool) -> GameResult<GameState> {
         let mut game = Game::new();
         game.regular_chess_setup();
 
+        let state = if stream.is_some() {
+            if client {
+                State::WaitingForOpponent
+            } else {
+                State::Playing
+            }
+        } else {
+            State::Playing
+        };
+
         let size = graphics::size(ctx);
         let min = size.0.min(size.1);
-        let s = GameState {
+        let mut s = GameState {
             game,
             selected_tile: None,
             move_buttons: Vec::with_capacity(10),
@@ -99,12 +126,18 @@ impl GameState {
             board_size: min,
             tile_size: min / 8.0,
             scale_factor: min / 1000.0,
+            state,
+            next_move: None,
+            stream,
         };
+
+        if let Some(stream) = &mut s.stream {
+            stream.set_nonblocking(true).unwrap();
+        }
+
         Ok(s)
     }
-}
 
-impl GameState {
     fn select_tile(&mut self, x: usize, y: usize) {
         let board = self.game.board();
 
@@ -120,10 +153,179 @@ impl GameState {
             }
         }
     }
+
+    fn handle_networking(&mut self) {
+        let mut buf: [u8; 5] = [0; 5];
+        let stream: &mut TcpStream = self.stream.as_mut().unwrap();
+        let len = match stream.read(&mut buf) {
+            Ok(len) => len,
+            Err(e) => {
+                if e.kind() != std::io::ErrorKind::WouldBlock {
+                    panic!("Something bad happend: {}", e);
+                }
+                0
+            }
+        };
+
+        if len == 0 {
+            return;
+        }
+
+        if len == 1 {
+            #[allow(dead_code)]
+            #[repr(u8)]
+            enum MessageType {
+                Decline = 0,
+                Undo = 2,
+                Accept = 3,
+                Checkmate = 4,
+                Draw = 5,
+                Resign = 6,
+            }
+
+            let message_type: MessageType = unsafe { std::mem::transmute(buf[0]) };
+
+            match message_type {
+                MessageType::Decline => self.state = State::Playing,
+                MessageType::Undo => {
+                    stream.write(&[1; 1]).unwrap();
+                }
+                MessageType::Draw => {
+                    stream.write(&[1; 1]).unwrap();
+                }
+                MessageType::Checkmate => self.state = State::GameOver,
+                MessageType::Resign => self.state = State::GameOver,
+                _ => {}
+            }
+            stream.flush().unwrap();
+        } else {
+            #[allow(dead_code)]
+            #[repr(u8)]
+            enum MoveType {
+                Standard = 0,
+                EnPassant = 1,
+                Promotion = 2,
+                KingsideCastle = 3,
+                QueensideCastle = 4,
+            }
+
+            #[derive(Copy, Clone)]
+            struct Standard {
+                origin: u8,
+                target: u8,
+            }
+
+            #[derive(Copy, Clone)]
+            struct Promotion {
+                origin: u8,
+                target: u8,
+                r#type: u8,
+            }
+
+            #[repr(C)]
+            union MoveData {
+                standard: Standard,
+                promotion: Promotion,
+            }
+
+            struct Move {
+                _padding: u8,
+                tag: MoveType,
+                data: MoveData,
+            }
+
+            let r#move: Move = unsafe { std::mem::transmute(buf) };
+
+            let to_coords = |pos: u8| {
+                let x = (pos & 0b111) as usize;
+                let y = (pos >> 3) as usize;
+                (x, y)
+            };
+
+            if let Some((from, to)) = self.next_move {
+                self.game.next(from, to);
+                self.next_move = None;
+            }
+
+            unsafe {
+                match r#move {
+                    Move {
+                        _padding: 1,
+                        tag: MoveType::Standard,
+                        data: MoveData { standard: data },
+                    } => {
+                        let origin = to_coords(data.origin);
+                        let target = to_coords(data.target);
+                        if let Some(moves) = self.game.moves_from(origin) {
+                            let r#type = moves[0].2;
+                            self.game.next(origin, (target.0, target.1, r#type));
+                        }
+                    }
+                    Move {
+                        _padding: 1,
+                        tag: MoveType::EnPassant,
+                        data: MoveData { standard: data },
+                    } => {
+                        let origin = to_coords(data.origin);
+                        let target = to_coords(data.target);
+                        if let Some(moves) = self.game.moves_from(origin) {
+                            let r#type = moves[0].2;
+                            self.game.next(origin, (target.0, target.1, r#type));
+                        }
+                    }
+                    Move {
+                        _padding: 1,
+                        tag: MoveType::Promotion,
+                        data: MoveData { promotion: data },
+                    } => {
+                        if data.r#type > 3 {
+                            stream.write(&[1; 1]).unwrap();
+                        }
+                        let r#type = match data.r#type {
+                            0 => PieceType::Knight,
+                            1 => PieceType::Bishop,
+                            2 => PieceType::Rook,
+                            _ => PieceType::Queen,
+                        };
+                        let origin = to_coords(data.origin);
+                        let target = to_coords(data.target);
+                        self.game.next(origin, (target.0, target.1, r#type));
+                    }
+                    _ => {}
+                }
+            }
+
+            self.state = State::Playing;
+        }
+    }
+
+    fn send_next_move(&mut self) {
+        let stream = self.stream.as_mut().unwrap();
+        let (from, to) = self.next_move.unwrap();
+
+        let from = (from.0 as u8, from.1 as u8);
+        let to = (to.0 as u8, to.1 as u8);
+
+        let to_index = |x: u8, y: u8| x | (y << 3);
+
+        let buffer: [u8; 4] = [1, 0, to_index(from.0, from.1), to_index(to.0, to.1)];
+
+        stream.write(&buffer).unwrap();
+        stream.flush().unwrap();
+        self.state = State::WaitingForOpponent;
+    }
 }
 
 impl event::EventHandler for GameState {
     fn update(&mut self, _ctx: &mut Context) -> GameResult {
+        if self.game.no_moves() {
+            return Ok(());
+        }
+
+        if self.state == State::WaitingForOpponent {
+            self.handle_networking();
+        }
+
         Ok(())
     }
 
@@ -134,7 +336,7 @@ impl event::EventHandler for GameState {
         x: f32,
         y: f32,
     ) {
-        if self.game.no_moves() {
+        if self.game.no_moves() || self.state != State::Playing {
             return;
         }
 
@@ -143,7 +345,12 @@ impl event::EventHandler for GameState {
             let y = (y / self.tile_size) as usize;
             for button in &self.move_buttons {
                 if button.inside(x, y) {
-                    self.game.next(button.from(), button.to());
+                    if self.stream.is_some() {
+                        self.next_move = Some((button.from(), button.to()));
+                        self.send_next_move();
+                    } else {
+                        self.game.next(button.from(), button.to());
+                    }
                     self.selected_tile = None;
                     self.move_buttons.clear();
 
@@ -223,6 +430,18 @@ impl event::EventHandler for GameState {
             )?;
         }
 
+        if self.state == State::WaitingForOpponent {
+            let (w, h) = graphics::size(ctx);
+            let rect = graphics::Mesh::new_rectangle(
+                ctx,
+                graphics::DrawMode::fill(),
+                graphics::Rect::new(0.0, 0.0, w, h),
+                graphics::Color::from_rgba(100, 100, 100, 150),
+            )?;
+
+            graphics::draw(ctx, &rect, graphics::DrawParam::new())?;
+        }
+
         if self.game.no_moves() {
             graphics::draw(
                 ctx,
@@ -247,9 +466,38 @@ pub fn main() -> GameResult {
 
     let mode: conf::WindowMode = Default::default();
     let cb = ggez::ContextBuilder::new("chess", "ggez")
-        .window_mode(mode.dimensions(1000.0, 1000.0))
+        .window_mode(mode.dimensions(500.0, 500.0))
+        .window_setup(conf::WindowSetup {
+            title: "Chess".to_owned(),
+            icon: "".to_owned(),
+            vsync: true,
+            srgb: true,
+            samples: conf::NumSamples::One,
+        })
         .add_resource_path(resource_dir);
     let (ctx, event_loop) = &mut cb.build()?;
-    let state = &mut GameState::new(ctx)?;
-    event::run(ctx, event_loop, state)
+
+    let mut args = env::args();
+    let _ = args.next();
+    let mut state = if let Some(arg) = args.next() {
+        if arg == "--host" {
+            let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+            let stream = listener.incoming().next().unwrap().unwrap();
+            GameState::new(ctx, Some(stream), false)?
+        } else if arg == "--client" {
+            if let Some(arg) = args.next() {
+                let stream = TcpStream::connect(arg).unwrap();
+                GameState::new(ctx, Some(stream), true)?
+            } else {
+                panic!("Expected ip address after --client");
+            }
+        } else {
+            panic!("Unknown command: {}", arg);
+        }
+    } else {
+        GameState::new(ctx, None, false)?
+    };
+
+    event::run(ctx, event_loop, &mut state)
 }
